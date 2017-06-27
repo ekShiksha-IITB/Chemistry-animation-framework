79e51515d8bd275f08be30ec3f21a5a4dc1c9b58
codher23 <shreyagovil23@gmail.com>
2017-06-27 12:05:24 +0530

    Added the structure of hydrocarbons.

---
+++ Chemistry2/src/java/Servlets/CompoundServlet.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import DAO.CompoundDAO;
+import Objects.Compound;
+
+/**
+ *
+ * @author shreya
+ */
+public class CompoundServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {           
+            try {   
+                int example = Integer.parseInt((String) request.getParameter("example"));
+                CompoundDAO alkanesDao = new CompoundDAO();
+                Compound alkanes = alkanesDao.getAlkanes(example);
+                request.setAttribute("alkanes", alkanes); // Will be available as ${alkanes} in JSP
+                request.getRequestDispatcher("/index.jsp?option=9").forward(request, response);
+            } 
+            catch (Exception e) {
+                e.printStackTrace();
+            } 
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
